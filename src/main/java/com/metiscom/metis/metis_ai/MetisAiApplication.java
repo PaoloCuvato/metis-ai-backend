0d6fa9cc688dda6a61190b9acf3d1cb41062b0b7
PaoloCuvato <paolocuvato95@gmail.com>
2026-09-14 14:42:04 +0200

    added first push

---
+++ src/main/java/com/metiscom/metis/metis_ai/MetisAiApplication.java
@@ -0,0 +1,13 @@
+package com.metiscom.metis.metis_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetisAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetisAiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/metiscom/metis/metis_ai/controller/ChatController.java
@@ -0,0 +1,27 @@
+package com.metiscom.metis.metis_ai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chat")
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping
+    public String askOllama(@RequestParam String prompt) {
+        return this.chatClient.prompt()
+                .user(prompt)
+                .call().
+                content();
+    }
+
+}
